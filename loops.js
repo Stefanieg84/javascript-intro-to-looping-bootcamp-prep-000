673cbefa48663c32e76c77da3788fe75c7114df9
Stefanieg84 <stefanieg84@gmail.com>
2018-08-23 17:41:50 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(array){
+  
+  for (let i = 0; i < 25; i++) {
+    if(i === 1){
+      array.push("I am 1 strange loop.");
+    } else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+}
+
+function whileLoop(number){
+  
+  while(number>0){
+    console.log(--number);
+  }
+  
+  return "done";
+}
+
+function doWhileLoop(array){
+  
+  function incrementVariable() {
+    i = i + 1;
+  }
+  
+  do {
+    array.pop()
+    incrementVariable();
+  } while (array.length > 0 && incrementVariable() === false);
+  
+  return array;
+}
